18ac68a59c9d33cd1ec818696b4b4dfe392da398
Denis Davydov <davydov.den18@gmail.com>
2014-06-16 19:04:41 +0300

    changed directory structure according to conventions

---
+++ user-Manager/src/main/java/com/softserveinc/ita/controller/impl/UserController.java
@@ -0,0 +1,35 @@
+package com.softserveinc.ita.controller.impl;
+
+import com.softserveinc.ita.entity.User;
+import com.softserveinc.ita.service.UserService;
+import com.softserveinc.ita.utils.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userServiceImpl;
+
+    @Autowired
+    private JsonUtil jsonUtil;
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> response = userServiceImpl.getUsers();
+        if (response == null || response.isEmpty()) {
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ user-Manager/src/test/java/com/softserveinc/ita/DAO/BaseDAOTest.java
@@ -0,0 +1,11 @@
+package com.softserveinc.ita.DAO;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/user-spring-config.xml"})
+public abstract class BaseDAOTest {
+
+}
