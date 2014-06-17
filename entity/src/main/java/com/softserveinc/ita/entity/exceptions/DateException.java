c61fdf6b7ab6554c7d8ec6019ecdd76934cae592
VadimNaumenko <vadim625@mail.ru>
2014-06-17 13:08:22 +0300

    Added DateValidation

---
+++ entity/src/main/java/com/softserveinc/ita/entity/exceptions/DateException.java
@@ -0,0 +1,18 @@
+package com.softserveinc.ita.entity.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Вадим
+ * Date: 17.06.14
+ * Time: 13:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class DateException extends Exception {
+
+    public DateException() {
+    }
+
+    public DateException(String message) {
+        super(message);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
