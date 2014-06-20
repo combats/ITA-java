5049f9c86e8690286b7ceb1b869897a16fd44f21
BraslavskiyAndrey <braslavskiyao@gmail.com>
2014-06-20 11:55:39 +0300

    test

---
+++ appointment-webui/src/main/java/com/softserveinc/ita/controlers/HelloController.java
@@ -0,0 +1,7 @@
+package com.softserveinc.ita.controlers;
+
+/**
+ * Created by Andrey on 19.06.2014.
+ */
+public class HelloController {
+}
---
+++ webui-appointments/src/main/java/com/softserveinc/ita/controller/MockController.java
@@ -0,0 +1,7 @@
+package com.softserveinc.ita.controller;
+
+/**
+ * Created by Andrey on 19.06.2014.
+ */
+public class MockController {
+}
