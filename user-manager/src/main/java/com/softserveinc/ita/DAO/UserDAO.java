c45abfeeac46ec3d9749bad5d722822591c4d110
Denis Davydov <davydov.den18@gmail.com>
2014-06-22 22:57:28 +0300

    Revert "Merge branch 'feature/userManager/getUsers' into DevelopCandidate2"
    This reverts commit dfc1093682e52cbc35445fd526804bd4ab894f66, reversing
    changes made to c60f46bd32f804b96b263d908203dd0d9aa9bcfc.

--- user-manager/src/main/java/com/softserveinc/ita/DAO/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.softserveinc.ita.DAO;
-
-import com.softserveinc.ita.entity.User;
-
-import java.util.List;
-
-public interface UserDAO {
-    List<User> getUsers();
-}
--- user-manager/src/main/java/com/softserveinc/ita/service/UserService.java
+++ user-manager/src/main/java/com/softserveinc/ita/service/UserService.java
@@ -4,8 +4,6 @@ import com.softserveinc.ita.entity.User;
 import com.softserveinc.ita.exception.InvalidUserIDException;
 import com.softserveinc.ita.exception.UserIDNotFoundUserDaoMockException;
 import java.util.ArrayList;
-import java.util.List;
-
 import com.softserveinc.ita.exception.EmptyUserException;
 import com.softserveinc.ita.exception.UserDoesNotExistException;
 
@@ -14,5 +12,5 @@ public interface UserService {
     ArrayList<String> getAllUsersID();
     void deleteUser(String userID) throws UserIDNotFoundUserDaoMockException;
     User editUser(User user) throws UserDoesNotExistException, EmptyUserException;
-    List<User> getUsers();
+
 }
