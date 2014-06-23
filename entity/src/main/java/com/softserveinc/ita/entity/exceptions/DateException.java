612f50c795f09a672131736cbee7bc8720929aea
Rossoha <nikolay.khoroshevskiy@gmail.com>
2014-06-23 20:01:39 +0300

    Add Nexus repository to pom
    Change encoding from ASCII to UTF-8

--- entity/src/main/java/com/softserveinc/ita/entity/exceptions/DateException.java
+++ entity/src/main/java/com/softserveinc/ita/entity/exceptions/DateException.java
@@ -1,12 +1,6 @@
 package com.softserveinc.ita.entity.exceptions;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Вадим
- * Date: 13.06.14
- * Time: 15:06
- * To change this template use File | Settings | File Templates.
- */
+
 public class DateException extends Exception {
 
     public DateException() {
