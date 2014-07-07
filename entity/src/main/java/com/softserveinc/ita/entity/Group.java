4e0dfa869fdb3ac88f2c04e46fcafe56c4dee1d4
BraslavskiyAndrey <abrastc@KVCLASS04.training.local>
2014-07-07 17:44:56 +0300

    get groups by status + web-ui was merged

--- entity/src/main/java/com/softserveinc/ita/entity/Group.java
+++ entity/src/main/java/com/softserveinc/ita/entity/Group.java
@@ -17,19 +17,6 @@ public class Group implements Serializable{
     public static final String BOARDING_STATUS = "Boarding";
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    @Column(name = "Id", unique = true)
-    private String groupID;
-
-    @ElementCollection
-    @CollectionTable(name = "Applicant", joinColumns = @JoinColumn(name = "Id"))
-    private List<Applicant> applicantsInGroup = new ArrayList<>();
-    private String groupName;
-    private Status groupStatus;
-    private Course course;
-
     public enum Status implements Serializable {
         IN_PROCESS(IN_PROCESS_STATUS), PLANNED(PLANNED_STATUS), OFFERING(OFFERING_STATUS), BOARDING(BOARDING_STATUS);
         private String name;
@@ -43,7 +30,20 @@ public class Group implements Serializable{
             this.name = name;
         }
     }
-    
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "Id", unique = true)
+    private String groupID;
+
+    @ElementCollection
+    @CollectionTable(name = "Applicant", joinColumns = @JoinColumn(name = "Id"))
+    private List<Applicant> applicantsInGroup = new ArrayList<>();
+    private String groupName;
+    private Status groupStatus;
+    private Course course;
+
     public Group() {}
 
     public Group(String groupID) {
@@ -79,10 +79,21 @@ public class Group implements Serializable{
 
     public void setApplicantsInGroup(List<Applicant> applicantsInGroup) {
         this.applicantsInGroup = applicantsInGroup;
+    }
     public Status getGroupStatus() {
         return groupStatus;
     }
 
+    @Override
+    public String toString() {
+        return "Group{" +
+                "groupID='" + groupID + '\'' +
+                ", applicantsInGroup=" + applicantsInGroup +
+                ", groupName='" + groupName + '\'' +
+                ", groupStatus=" + groupStatus +
+                ", course=" + course +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -91,25 +102,22 @@ public class Group implements Serializable{
 
         Group group = (Group) o;
 
-        if (applicantsInGroup != null ? !applicantsInGroup.equals(group.applicantsInGroup) : group.applicantsInGroup != null)
-            return false;
-        if (groupID != null ? !groupID.equals(group.groupID) : group.groupID != null) return false;
+        if (!applicantsInGroup.equals(group.applicantsInGroup)) return false;
+        if (!course.equals(group.course)) return false;
+        if (!groupID.equals(group.groupID)) return false;
+        if (!groupName.equals(group.groupName)) return false;
+        if (groupStatus != group.groupStatus) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = groupID != null ? groupID.hashCode() : 0;
-        result = 31 * result + (applicantsInGroup != null ? applicantsInGroup.hashCode() : 0);
+        int result = groupID.hashCode();
+        result = 31 * result + applicantsInGroup.hashCode();
+        result = 31 * result + groupName.hashCode();
+        result = 31 * result + groupStatus.hashCode();
+        result = 31 * result + course.hashCode();
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Group{" +
-                "groupID='" + groupID + '\'' +
-                ", applicantsInGroup=" + applicantsInGroup +
-                '}';
     }
 }
