9fcf983122404044fe99394a697a2a4103f0f4c0
BraslavskiyAndrey <abrastc@KVCLASS04.training.local>
2014-07-08 11:45:34 +0300

    error

--- mail-manager/src/main/java/com/softserveinc/ita/controller/MailerTest.java
+++ mail-manager/src/main/java/com/softserveinc/ita/controller/MailerTest.java
@@ -1,13 +1,14 @@
 package com.softserveinc.ita.controller;
 
 import com.softserveinc.ita.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 
 public class MailerTest {
-
 
     public static void main(String[] args) throws MessagingException {
         ApplicationContext context = new FileSystemXmlApplicationContext("classpath:spring-config.xml");
