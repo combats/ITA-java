c9b5c7d25993775fcb78ad0d636c55ac3c40111a
Mazich <mskrynnikov@gmail.com>
2014-07-18 14:29:02 +0300

    Added annotation @Transactional for db-transactions and level of isolation

--- applicant-manager/src/main/java/com/softserveinc/ita/service/impl/ApplicantServiceImpl.java
+++ applicant-manager/src/main/java/com/softserveinc/ita/service/impl/ApplicantServiceImpl.java
@@ -6,6 +6,8 @@ import com.softserveinc.ita.exception.ApplicantDoesNotExistException;
 import com.softserveinc.ita.service.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,14 +18,19 @@ public class ApplicantServiceImpl implements ApplicantService {
     private ApplicantDAO applicantDao;
 
     @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public List<Applicant> getApplicants() {
         return applicantDao.getApplicants();
     }
 
+    @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public List<Applicant> getApplicantsByGroupID(String groupID) {
         return applicantDao.getApplicantsByGroupID(groupID);
     }
 
+    @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public Applicant getApplicantById(String applicantId) throws ApplicantDoesNotExistException {
         Applicant searchedApplicant = applicantDao.getApplicantById(applicantId);
         if (searchedApplicant == null) {
@@ -32,15 +39,20 @@ public class ApplicantServiceImpl implements ApplicantService {
         return searchedApplicant;
     }
 
+    @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public Applicant addNewApplicant(Applicant applicant) {
         return applicantDao.addNewApplicant(applicant);
     }
 
+    @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public Applicant editApplicant(Applicant applicant) {
         return applicantDao.editApplicant(applicant);
     }
 
     @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public List<String> getApplicantIDList() {
         List<Applicant> applicants = applicantDao.getApplicants();
         if (applicants == null || applicants.isEmpty()) {
