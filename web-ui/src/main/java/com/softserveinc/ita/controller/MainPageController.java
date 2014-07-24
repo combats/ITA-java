da3824a06887ec90d2df4c4d68b74bbc2418e715
syperia <syperia@gmail.com>
2014-07-24 15:45:24 +0300

    UI LOGIN PAGWE BLA BLA

--- web-ui/src/main/java/com/softserveinc/ita/controller/MainPageController.java
+++ web-ui/src/main/java/com/softserveinc/ita/controller/MainPageController.java
@@ -3,6 +3,7 @@ package com.softserveinc.ita.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -11,4 +12,12 @@ public class MainPageController {
 	public String showMainPage() {
 		return "main";
 	}
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public ModelAndView showLoginPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login/login");
+        mav.addObject("error", "Wrong username/password combination!");
+        return mav;
+    }
 }
