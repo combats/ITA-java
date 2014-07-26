cf34bb5475086659e07ae378ac74f8dd920e2de5
Denis Davydov <davydov.den18@gmail.com>
2014-07-26 19:59:04 +0300

    enrich_db

--- enrich_DB.sql
+++ enrich_DB.sql
@@ -11,13 +11,6 @@ INSERT INTO interview.Users (Id, Active, Email, Name, Password, Phone, Surname, 
 INSERT INTO interview.Users (Id, Active, Email, Name, Password, Phone, Surname, role_Id) VALUES ('40288199477288a50147729125b00000', 1, 'dod@dod.com', 'Douche', 'eed346aede1c64193b74d7264a809c3e', '0634942253', 'Bag', 1);
 
 INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('1', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('2', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('3', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('4', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('5', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('6', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('7', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('8', 'Joe@gmail.com', 'Terison', 'Joe', 'Terison');
 
 INSERT INTO interview.Course (Id, ImageReference, Name) VALUES ('297ea2cd476e53d001476e540a5e0111', 'TestRefOne2', 'TestCourseOne1');
 INSERT INTO interview.Course (Id, ImageReference, Name) VALUES ('297ea2cd476e53d001476e540a5f0012', 'TestRefTwo', 'TestCourseTwo');
