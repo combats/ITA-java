a218a85dd0e1707346c6f126e0c3337ef1ae6da7
Rossoha <nikolay.khoroshevskiy@gmail.com>
2014-07-27 19:45:21 +0300

    Change favicon.icon + sql create tables

--- populate.sql
+++ populate.sql
@@ -8,14 +8,14 @@ INSERT INTO interview.Users (Id, Active, Email, Name, Password, Phone, Surname, 
 INSERT INTO interview.Users (Id, Active, Email, Name, Password, Phone, Surname, role_Id) VALUES ('40288199476e716901476e7511360005', 1, 'voryi3@ybivai.com', 'Alla', 'eed346aede1c64193b74d7264a809c3e', '0634942253', 'Cruper', '1');
 INSERT INTO interview.Users (Id, Active, Email, Name, Password, Phone, Surname, role_Id) VALUES ('40288199477288a50147729125b00000', 1, 'dod@dod.com', 'Douche', 'eed346aede1c64193b74d7264a809c3e', '0634942253', 'Bag', 1);
 
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('1', 'Joe@gmail.com', 'id1', 'Joe1', 'Terison1');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('2', 'ivan@gmail.com', 'id1', 'Joe2', 'Terison2');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('3', 'rastaman@gmail.com', 'id1', 'Joe3', 'Terison3');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('4', 'vasiliy@gmail.com', 'id1', 'Joe4', 'Terison4');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('5', 'nikolay@gmail.com', 'id1', 'Joe5', 'Terison5');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('6', 'denis@gmail.com', 'id1', 'Joe6', 'Terison6');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('7', 'andrey@gmail.com', 'id1', 'Joe7', 'Terison7');
-INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('8', 'anton@gmail.com', 'id1', 'Joe8', 'Terison8');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('1', 'lefft@ya.ru', '297ea2cd47737b6c0147737b8ced0001', 'Anton', 'Kravchuk');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('2', 'vasya@pupkin.su', '297ea2cd47737b6c0147737b8ced0002', 'Vasya', 'Pupkin');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('3', 'rastaman@gmail.com', '297ea2cd47737b6c0147737b8ced0003', 'Jozef', 'Jonson');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('4', 'vasiliy@gmail.com', '297ea2cd47737b6c0147737b8ced0004', 'Harison', 'Ford');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('5', 'nikolay@gmail.com', '297ea2cd47737b6c0147737b8ced0003', 'Mary', 'Bloody');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('6', 'denis@gmail.com', '297ea2cd47737b6c0147737b8ced0003', 'Jecob', 'Tornnnntoooon');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('7', 'andrey@gmail.com', '297ea2cd47737b6c0147737b8ced0003', 'Francis', 'Copppola');
+INSERT INTO interview.Applicants (Id, Email, GroupId, Name, Surname) VALUES ('8', 'anton@gmail.com', '297ea2cd47737b6c0147737b8ced0003', 'Billy', 'Jeam');
 
 INSERT INTO interview.Course (Id, ImageReference, Name) VALUES ('297ea2cd476e53d001476e540a5e0111', 'pen-java.png', 'Java');
 INSERT INTO interview.Course (Id, ImageReference, Name) VALUES ('297ea2cd476e53d001476e540a5f0012', 'pen-net.png', '.NET');
@@ -28,6 +28,13 @@ INSERT INTO interview.Groups (Id, Address, Capacity, EndTime, GroupName, StartBo
 INSERT INTO interview.Groups (Id, Address, Capacity, EndTime, GroupName, StartBoardingTime, StartTime, course_Id) VALUES ('297ea2cd47737b6c0147737b8ced0002', 'Degtya', 0, 0, 'ITA-005', 0, 0, '297ea2cd476e53d001476e540a5f0012');
 INSERT INTO interview.Groups (Id, Address, Capacity, EndTime, GroupName, StartBoardingTime, StartTime, course_Id) VALUES ('297ea2cd47737b6c0147737b8ced0003', 'Degtya', 0, 0, 'ITA-006', 0, 0, '297ea2cd476e53d001476e540a600013');
 INSERT INTO interview.Groups (Id, Address, Capacity, EndTime, GroupName, StartBoardingTime, StartTime, course_Id) VALUES ('297ea2cd47737b6c0147737b8ced0004', 'Degtya', 0, 0, 'ITA-007', 0, 0, '297ea2cd476e53d001476e540f350015');
+
+
+INSERT INTO interview.Appointments (Id, ActualStartTime, ApplicantId, DurationTime, GroupId, StartTime) VALUES ('4028819947789da3014778a3bf850000', 0, '1', 1800000, '297ea2cd47737b6c0147737b8ced0001', 1407672000000);
+INSERT INTO interview.Appointments (Id, ActualStartTime, ApplicantId, DurationTime, GroupId, StartTime) VALUES ('4028819947789da3014778a3eb8c0001', 0, '8', 1800000, '297ea2cd47737b6c0147737b8ced0001', 1407675600000);
+INSERT INTO interview.Appointments (Id, ActualStartTime, ApplicantId, DurationTime, GroupId, StartTime) VALUES ('4028819947789da3014778a4019e0002', 0, '6', 1800000, '297ea2cd47737b6c0147737b8ced0001', 1407679200000);
+INSERT INTO interview.Appointments (Id, ActualStartTime, ApplicantId, DurationTime, GroupId, StartTime) VALUES ('4028819947789da3014778a416b70003', 0, '3', 1800000, '297ea2cd47737b6c0147737b8ced0001', 1407682800000);
+INSERT INTO interview.Appointments (Id, ActualStartTime, ApplicantId, DurationTime, GroupId, StartTime) VALUES ('4028819947789da3014778a6d52d0004', 0, '4', 1800000, '297ea2cd47737b6c0147737b8ced0001', 1407686400000);
 
 
 INSERT INTO interview.ApplicantsWithStatus (Id, ApplicantsStatus, ApplicantId) VALUES ('297ea2cd47737b6c0147737b8ced0001', 0xACED00057372002E636F6D2E736F66747365727665696E632E6974612E656E746974792E4170706C6963616E7442656E63686D61726B17646B041C62E66002000249000472616E6B4C000673746174757374002E4C636F6D2F736F66747365727665696E632F6974612F656E746974792F4170706C6963616E74245374617475733B7870000000027E72002C636F6D2E736F66747365727665696E632E6974612E656E746974792E4170706C6963616E742453746174757300000000000000001200007872000E6A6176612E6C616E672E456E756D00000000000000001200007870740008454D504C4F594544, '1');
