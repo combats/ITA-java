408924c47d4b811da6cd079067bdfef96068d210
syperia <syperia@gmail.com>
2014-07-29 02:09:30 +0300

    UI additions

--- web-ui/src/main/webapp/WEB-INF/interview/components/header-menu/final-comment/finalComSvc.js
+++ web-ui/src/main/webapp/WEB-INF/interview/components/header-menu/final-comment/finalComSvc.js
@@ -1,8 +1,7 @@
 angular.module('finalComMod').factory('Comment',['$http', function($http) {
     return {
-        add: function(){
-            return $http({method: 'POST', url: "/inteviews/?????/" ,data:null});
-
+        update: function(fc){
+            q.interviewId = Appointment.appointmentId;
+            return $http({method: 'PUT', url: "http://176.36.11.25:8080/interviewing/final_comment/", data : fc});
         }
-
     }}])
--- web-ui/src/main/webapp/WEB-INF/interview/components/questions/questionsCtrl.js
+++ web-ui/src/main/webapp/WEB-INF/interview/components/questions/questionsCtrl.js
@@ -1,7 +1,7 @@
 (function () {
     var questionModule = angular.module('questionMod', []);
 
-    questionModule.controller('QuestionsController', ['$scope','Question','User', function ($scope, Questions, User) {
+    questionModule.controller('QuestionsController', ['$scope','Question','User', function ($scope, Question, User) {
         $scope.question = angular.copy(question);
         $scope.questions = User.questions;
 
@@ -17,19 +17,28 @@
         };
         $scope.submitQuestion = function(q){
             if($scope.isNewQuestion(q)){
-                $scope.questions.push(angular.copy(q));
-                //TODO: Question.add(q).then(...;
+                Question.add(q).then(function (response) {
+                    q.id = response;
+                    $scope.questions.push(angular.copy(q));
+                },function(err){
+                    alert("Error during question submit. Check the connection");
+                });
             }
             else if($scope.isAnyChangesQuestion(q)){
                 var i=0, len=$scope.questions.length;
                 for (; i<len; i++) {
-                if(q.question === $scope.questions[i].question){
-                    $scope.questions[i].comment = angular.copy(q.comment);
-                    $scope.questions[i].mark = q.mark;
-                    $scope.questions[i].weight = q.weight;
+                    if (q.question === $scope.questions[i].question) {
+                        $scope.questions[i].comment = angular.copy(q.comment);
+                        $scope.questions[i].mark = q.mark;
+                        $scope.questions[i].weight = q.weight;
+                        Question.update(q).then(function (response) {
+
+                        }, function (err) {
+                            alert("Error during question update. Check the connection");
+                        });
+                    }
                 }
-                }
-                //TODO: Question.update(q.question, q).then(...;
+
             }
         };
         $scope.intToWeight = function(num){
