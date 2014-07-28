4b5f12084748089dcaee864349a53b2d29e3c35b
Anton <ynotleft@gmail.com>
2014-07-28 18:15:43 +0300

    minor changes in logger, changed applicant-notification

--- applicants-notification/src/main/java/com/softserveinc/ita/controller/ApplicantsNotificationController.java
+++ applicants-notification/src/main/java/com/softserveinc/ita/controller/ApplicantsNotificationController.java
@@ -2,6 +2,7 @@ package com.softserveinc.ita.controller;
 
 import com.softserveinc.ita.entity.NotificationJSONInfo;
 import com.softserveinc.ita.service.QueueManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,13 +15,15 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class ApplicantsNotificationController {
-
+    static Logger log = Logger.getLogger(
+            ApplicantsNotificationController.class.getName());
     @Autowired
     private QueueManager queueManagerService;
 
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody List<NotificationJSONInfo> postIDEntityListToNotifyApplicants(@RequestBody List<NotificationJSONInfo> infoList) {
         queueManagerService.notifyApplicants(infoList);
+        log.info("IDs: " + infoList + " was posted to the queue");
         return infoList;
     }
 }
