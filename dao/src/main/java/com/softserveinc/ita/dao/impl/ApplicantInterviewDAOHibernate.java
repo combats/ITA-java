35278725a31baa8682e8aa5588a8e9ddc205d727
Mazich <mskrynnikov@gmail.com>
2014-07-29 17:57:33 +0300

    Added methods in ApplicantInterviewDAOHibernate and covered with tests

--- dao/src/main/java/com/softserveinc/ita/dao/impl/ApplicantInterviewDAOHibernate.java
+++ dao/src/main/java/com/softserveinc/ita/dao/impl/ApplicantInterviewDAOHibernate.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Repository
 public class ApplicantInterviewDAOHibernate implements ApplicantInterviewDAO {
@@ -21,4 +22,31 @@ public class ApplicantInterviewDAOHibernate implements ApplicantInterviewDAO {
 		Serializable id = currentSession.save(applicantInterview);
 		return (ApplicantInterview) currentSession.get(ApplicantInterview.class, id);
 	}
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<ApplicantInterview> getAllInterview() {
+        return (List<ApplicantInterview>) sessionFactory.getCurrentSession()
+                .createCriteria(ApplicantInterview.class).list();
+    }
+
+    @Override
+    public ApplicantInterview getInterviewById(String id) {
+        return (ApplicantInterview) sessionFactory.getCurrentSession().get(ApplicantInterview.class, id);
+    }
+
+    @Override
+    public ApplicantInterview updateInterview(ApplicantInterview applicantInterview) {
+        Session session = sessionFactory.getCurrentSession();
+        session.update(applicantInterview);
+        return (ApplicantInterview) session.get(ApplicantInterview.class, applicantInterview.getId());
+    }
+
+    @Override
+    public ApplicantInterview deleteInterviewById(String id) {
+        Session session = sessionFactory.getCurrentSession();
+        ApplicantInterview interviewOnDelete = (ApplicantInterview) session.get(ApplicantInterview.class, id);
+        session.delete(interviewOnDelete);
+        return interviewOnDelete;
+    }
 }
