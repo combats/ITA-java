9c38d7d14c81466f3a83031bb640e8737527e430
Vlad <Vlad@gmail.com>
2014-07-29 18:17:23 +0300

    groupId

--- web-ui/src/main/webapp/WEB-INF/interview/components/header-menu/final-comment/finalComCtrl.js
+++ web-ui/src/main/webapp/WEB-INF/interview/components/header-menu/final-comment/finalComCtrl.js
@@ -1,16 +1,25 @@
 var  mediapp = angular.module('finalComMod',[]);
-mediapp.controller('finalComCtrl',['$scope','Comment', 'User', '$rootScope', function($scope, Comment, User, $rootScope){
+mediapp.controller('finalComCtrl',['$scope','Comment', 'User', 'Appointment', '$rootScope','$http', function($scope, Comment, User, Appointment, $rootScope, $http){
 
         $scope.user = User;
         $scope.finalComment = '';
 
-
+    var applicantId = Appointment.applicantId;
+    var baseUrl = "http://176.36.11.25:8080/";
+    var $cookies = angular.injector(['ngCookies']).get('$cookies');
+    var groupId = $cookies.groupID;
+    var list = {applicantId:{"status":"NOT_PASSED", "rank":-1}};
 
     $scope.submitComment = function(){
         var finalComment = {};
         finalComment.finalComment = $scope.finalComment;
 
-        $rootScope.$broadcast('New question was added');
+        $rootScope.$broadcast('Final Comment was submit');
+
+        $http({method: 'PUT',
+               url: baseUrl+'/groups/' + groupId + '/applicants',
+               data: JSON.stringify(list)
+                 });
 
 //        Comment.update(finalComment).then(function(response){
 //                //yoohoo, you completed interview!
